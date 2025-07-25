e9fe6cc3a44c1c34c3688943b3ffe3415b30db1f
Ken Española <revas4192@gmail.com>
2025-07-25 23:34:38 +0800

    Update renderer.js

--- src/renderer.js
+++ src/renderer.js
@@ -24,7 +24,7 @@ async function getVideos(query, isNewSearch = true) {
     try {
         const data = await window.electronAPI.youtubeSearch(query, nextPageToken);
         if (isNewSearch) videoList.innerHTML = "";
-
+         
         if (!data || !data.items || data.items.length === 0) {
             if (isNewSearch) videoList.innerHTML = "No results.";
             return;
